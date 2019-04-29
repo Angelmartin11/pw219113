5feb91cce1d8a7fdc0c0e85c18f38601b4a9da4c
Angelmartin11 <angelmartin11@icloud.com>
2019-04-29 13:59:35 -0600

    Conexion a la bd

--- crud_node/src/app.js
+++ crud_node/src/app.js
@@ -1,7 +1,26 @@
 const express = require('express');
 const app = express();
+const path= require('path');
+const mysql=require('mysql');
+const myConnection=require('express-myconnection');
+
+
 //Importamos las rutas 
 const indiceRutas=require('./rutas/index');
+
+//Configuraciones
+app.set('port',process.env.PORT || 3000);
+app.set('view engine', 'ejs');
+app.set('views',path.join(__dirname,'views'));
+
+//Middleware
+app.use(myConnection(mysql,{
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	port: 3306,
+	database: 'crudnodejsmysql'
+},'single'));
 
 //Rutas
 app.use("/", indiceRutas);
