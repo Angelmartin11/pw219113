f330b04040a81afaffcd3192f60011acb8950d1e
Angelmartin11 <angelmartin11@icloud.com>
2019-05-06 14:03:08 -0600

    Inicio Practica Crud_Node

--- crud_node/src/rutas/index.js
+++ crud_node/src/rutas/index.js
@@ -1,8 +1,11 @@
 const express=  require('express');
 const rutas= express.Router();
 
-rutas.get("/", (req,res)=>{
-	res.send("Hola mundo pero en node jajaja que roio perro");
-});
+const customerController = require('../controllers/customerController');
+
+// rutas.get("/", (req,res)=>{
+// 	res.send("Hola mundo pero en node jajaja que roio perro");
+// });
+rutas.get('/',customerController.list);
 
 module.exports = rutas;
