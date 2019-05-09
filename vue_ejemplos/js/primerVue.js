516aa4e853c38efcadf36d33874c8b1da347c771
Angelmartin11 <angelmartin11@icloud.com>
2019-05-09 13:59:50 -0600

    Fruta2 Vue.js

--- vue_ejemplos/js/primerVue.js
+++ vue_ejemplos/js/primerVue.js
@@ -8,6 +8,35 @@ const app = new Vue({
 			{nombre: 'Uva',cantidad: 0},
 			{nombre: 'Peras',cantidad: 80},
 			{nombre: 'Fresas',cantidad: 110},
-		]
+		],
+		nuevaFruta: '',
+		total: 0
+	},
+	methods:{
+		agregarFruta(){
+			//console.log("diste click");
+			if(this.nuevaFruta != ''){
+				this.frutas.push(this.nuevaFruta);
+				this.frutas2.push({
+					nombre: this.nuevaFruta,
+					cantidad: 0
+				});
+				this.nuevaFruta ='';
+			}
+		}
+
+		// agregaCant(){
+		// 	this.fruta.push()
+		// }
+	},
+	computed: {
+		sumarFrutas(){
+			this.total=0;
+			for(fruta of this.frutas2){
+				this.total=this.total+fruta.cantidad;
+			}
+			return this.total;
+		}
 	}
+
 });
